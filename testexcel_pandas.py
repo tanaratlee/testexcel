dffd14257395d0626ff9b539b4285321358708c7
Tanarat <tanaratlee@gmail.com>
2024-10-24 23:51:06 +0700

    modify testexcel_pandas by iloc search with index

--- testexcel_pandas.py
+++ testexcel_pandas.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-df = pd.read_excel('CustIDtoLineID.xlsx',usecols='A,B')
-print(df.dtypes)
-print(df.CustomerID)
+df = pd.read_excel('CustIDtoLineID.xlsx',sheet_name='CustIDtoLineID',usecols='A,B')
+#print(df.dtypes)
+#print(df.CustomerID)
+#print(df.iloc[:,0])
 
-print(df.loc[df.CustomerID=='999'].LineID)
-df.CustomerID = df.CustomerID.astype("string")
+#print(df.loc[df.CustomerID=='999'].LineID)
+#df.CustomerID = df.CustomerID.astype("string")
+print(df[df.columns[0:2]])
+df[df.columns[0:2]] = df[df.columns[0:2]].astype("string")
 print(df.dtypes)
 print(df.loc[df.CustomerID=='999'].LineID)
+
+print(df.loc[df.iloc[:,0]=='999'].iloc[0,1])
+print(df.loc[df.iloc[:,0]=='99'])
+
+if df.loc[df.iloc[:,0]=='99'].empty : print("None")
+else: print("value")
